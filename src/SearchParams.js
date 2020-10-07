b45e1859e4c2768a46beb6c9921fcd838ab78b71
Kato <kubkate@gmail.com>
2020-10-07 14:59:09 -0700

    add useEffect to run an API call

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [BREEDS, setBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "All", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", BREEDS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // run only if there is change in dependecies-> animal
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+
+    setBreeds([]);
+    setBreed("");
+
+    // make an asyncronous API call to get breeds, then setBreeds
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
