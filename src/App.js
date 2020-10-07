2b2c585dc046764b66a379bd54b3b149ec3842b6
Kato <kubkate@gmail.com>
2020-10-06 20:38:05 -0700

    Create SearchParams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Mixed" />
-      <Pet name="Pepper" animal="Cat" breed="Mixed" />
-      <Pet name="Waea" animal="Bird" breed="Mixed" />
+
+      <SearchParams />
     </div>
   );
 };
