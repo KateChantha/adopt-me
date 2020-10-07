d6fb5cedee5653a287eb715dd8e40e23650fc156
Kato <kubkate@gmail.com>
2020-10-06 17:57:57 -0700

    setup  prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ])
-}
-
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, [
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Mixed'
+      name: "Luna",
+      animal: "Dog",
+      breed: "Mixed",
     }),
     React.createElement(Pet, {
-      name: 'WAwa',
-      animal: 'cat',
-      breed: 'Mixed'
+      name: "WAwa",
+      animal: "cat",
+      breed: "Mixed",
     }),
     React.createElement(Pet),
-  ]
+  ]);
+};
 
-  );
-}
-
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
