a8a5ac7554e99f91a62c630c690ca430fed3dd50
Kato <kubkate@gmail.com>
2020-10-12 14:45:51 -0700

    add requestPets function to get pets as user submit form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -6,17 +6,31 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "All", ANIMALS);
+  // useDropdown() will return [state, Dropdown, setState]
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
 
-  // run only if there is change in dependecies-> animal
+  async function requestPets() {
+    const { animals } = await pet.animal({
+      location,
+      breed,
+      type: animal,
+    });
+
+    // if there is no animals data come back, then set pets to emtpy array
+    setPets(animals || []);
+  }
+
+  // run only if there is change in dependecies-> animal state in Animal dropdown
   useEffect(() => {
     // pet.breeds("dog").then(console.log, console.error);
 
     setBreeds([]);
     setBreed("");
 
-    // make an asyncronous API call to get breeds, then setBreeds
+    // asynchronously request data from the Petfinder API to get breeds, then setBreeds
     pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      // iterate over each object apiBreeds to get name of type value
       const breedStrings = apiBreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
@@ -24,7 +38,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
