35be8cc4146289007d0a8030994986314534cc39
Kato <kubkate@gmail.com>
2020-10-22 19:39:19 -0700

    fix bugs at <select value={state}>

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -14,7 +14,7 @@ const useDropdown = (label, defaultState, options) => {
       {label}
       <select
         id={id}
-        // value={state} // not sure why we need value attribute in<select>, thought we get e.target.value out of value of <option>
+        value={state} /** must  */
         onChange={(e) => setState(e.target.value)}
         onBlur={(e) => setState(e.target.value)}
         disabled={!options.length}
