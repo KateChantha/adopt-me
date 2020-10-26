3be999aa37fca46aab9e6edfb9efee3bec987e53
Kato <kubkate@gmail.com>
2020-10-26 13:30:01 -0700

    add css in js

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,16 +2,24 @@ import React from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">🐥</span>
-  </header>
-);
+const NavBar = () => {
+  const primarycolor = "pink";
+  
+  return (
+    <header
+      css={css`
+        background-color: ${primarycolor};
+        padding: 1rem;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span 
+        role="img" 
+        aria-label="logo"
+        css={css`font-size: 4rem`}
+      >🐥</span>
+    </header>
+  );
+} 
 
 export default NavBar;
